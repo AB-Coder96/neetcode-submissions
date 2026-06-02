ee4133e4badca6f6e16b9cbfe8fbb436443a2b0d
ABcoders <143926417+AB-Coder96@users.noreply.github.com>
2026-06-02 04:12:44 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    std::vector<int> twoSum(const std::vector<int>& nums, int target) {
+       constexpr size_t CAPACITY = 2048;
+        constexpr size_t MASK = CAPACITY - 1;
+        alignas(64) int keys[CAPACITY];
+        alignas(64) int values[CAPACITY];
+        alignas(64) bool occupied[CAPACITY] = {false};
+        const int* data_ptr = nums.data();
+        const size_t len = nums.size();
+
+        for (size_t i = 0; i < len; ++i) {
+            int current_num = data_ptr[i];
+            int complement = target - current_num;
+            size_t hash_idx = (static_cast<size_t>(complement) * 2654435761U) & MASK;
+            while (occupied[hash_idx]) {
+                if (keys[hash_idx] == complement) [[unlikely]] {
+                    return {values[hash_idx], static_cast<int>(i)};
+                }
+                hash_idx = (hash_idx + 1) & MASK;
+            }
+            size_t insert_idx = (static_cast<size_t>(current_num) * 2654435761U) & MASK;
+            while (occupied[insert_idx]) {
+                insert_idx = (insert_idx + 1) & MASK;
+            }
+            keys[insert_idx] = current_num;
+            values[insert_idx] = static_cast<int>(i);
+            occupied[insert_idx] = true;
+        }
+
+        return {};
+    }
+};
